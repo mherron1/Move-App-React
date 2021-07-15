cd5c516c08a257a893b95d1df4cd0857868e7702
mherron1 <mherron2021@gmail.com>
2021-07-15 01:36:59 +0100

    initial commit

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -4,7 +4,7 @@ import './Movie.css';
 const IMAGEPATH = "https://image.tmdb.org/t/p/w500";
 
 
-const Movie = ({title, poster_path, vote_average })=>{
+const Movie = ({title, poster_path, vote_average, overview })=>{
 
     let className = "red";
     if(vote_average > 6.5){
@@ -17,10 +17,12 @@ const Movie = ({title, poster_path, vote_average })=>{
 
     return (
         <div className ="movie">
+
+        <div className="overview">{overview}</div>
          <img className="poster" src={IMAGEPATH + poster_path}></img>
    
                 <div className="info">
-                <h3>{title}</h3>
+                <h4>{title}</h4>
                     <p className={className}>{vote_average}</p>
                     
                 </div>
